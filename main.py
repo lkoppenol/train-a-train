ccf55873cd45a2bdc5ae7dcf024e2e84fad1facb
lkoppenol <lkoppenol@gmail.com>
2019-05-05 14:48:38 +0200

    remove all but best

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import pygame
 
 def main():
     track = Environment('track.png')
-    game_engine = Engine(track, [])
+    game_engine = Engine(track, [HumanPlayer()])
     game_engine.bind_action(pygame.K_9, lambda: game_engine.add_player(Ai()))
     game_engine.bind_action(pygame.K_0, lambda: new_player(game_engine))
-    game_engine.bind_action(pygame.K_q, lambda: game_engine.remove_all_players())
+    game_engine.bind_action(pygame.K_q, lambda: game_engine.remove_all_players(keep=[game_engine.get_best_player()]))
     game_engine.play(False)
 
 
